ac58c17138a8e23dc3a820e46221319f2d9b67bd
PizzalaFlorian <florian.pizzala@gmail.com>
2017-01-11 13:53:03 +0100

    services & getter

---
+++ flo/src/fr/uga/miashs/album/model/Picture.java
@@ -0,0 +1,70 @@
+package fr.uga.miashs.album.model;
+
+import java.net.URI;
+import java.nio.file.Path;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Picture {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@ManyToOne
+	private Album album;
+	
+	@NotNull
+	private String title;
+	
+	@NotNull
+	private URI uri;
+	
+	@NotNull
+	private Path localfile;
+	
+	public Picture(){
+		
+	}
+
+	public Album getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(Album album) {
+		this.album = album;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public URI getUri() {
+		return uri;
+	}
+
+	public void setUri(URI uri) {
+		this.uri = uri;
+	}
+
+	public Path getLocalfile() {
+		return localfile;
+	}
+
+	public void setLocalfile(Path localfile) {
+		this.localfile = localfile;
+	}
+	
+	
+	
+}
